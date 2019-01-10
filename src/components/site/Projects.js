4721499c00ec7b74f7ccf6bbe3dd4a5247364da7
Xurmein <wj.pavocrist@gmail.com>
2019-01-10 14:53:52 -0500

    styling-profiling-organizing...intergalactic, planetary

--- src/components/site/Projects.js
+++ src/components/site/Projects.js
@@ -2,20 +2,22 @@ import React from 'react';
 import '../../App.css'
 
 const Projects = () => (
-    <section className="projects-container">
+    <div className="projects-container">
         <h2 className="projects-title">Example Work</h2>
-        <div className="projects-grid">
-            <div className="yelp"></div>
-            <div className="dokit"></div>
-            <div className="angular"></div>
-            <div className="cosmoknot"></div>
-            <div className="nytVidSearch"></div>
-            <div className="cheatBeat"></div>
-            <div className="jsTutorials"></div>
-            <div className="cssCreature"></div>
-            <div className="gitHub"></div>
-        </div>
-    </section>
+        <section className="projects-container">
+            <div className="projects-grid">
+                <div className="yelp"></div>
+                <div className="dokit"></div>
+                <div className="angular"></div>
+                <div className="cosmoknot"></div>
+                <div className="nytVidSearch"></div>
+                <div className="cheatBeat"></div>
+                <div className="jsTutorials"></div>
+                <div className="cssCreature"></div>
+                <div className="gitHub"></div>
+            </div>
+        </section>
+    </div>
 );
 
 export default Projects;
