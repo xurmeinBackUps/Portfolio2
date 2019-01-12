747d6c446adf43d972ebfffe023a074168f7d1ed
Xurmein <wj.pavocrist@gmail.com>
2019-01-12 16:11:04 -0500

    Updates

---
+++ precache-manifest.a991dd2c14182ca449d921330a3de090.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "51b9c4f0ec863c336f0d425b1a3aa2e1",
+    "url": "/Portfolio2/static/media/solar-system.51b9c4f0.png"
+  },
+  {
+    "revision": "1e042664a3be2d9a7541d9d15b56b7cf",
+    "url": "/Portfolio2/static/media/picassoAtTheLapinAgile.1e042664.jpg"
+  },
+  {
+    "revision": "c255c1490db26f040536524c5b90e951",
+    "url": "/Portfolio2/static/media/kraken.c255c149.png"
+  },
+  {
+    "revision": "cb8cecb81e23c0318db8",
+    "url": "/Portfolio2/static/js/runtime~main.cb8cecb8.js"
+  },
+  {
+    "revision": "6009e555e7a58898f77d",
+    "url": "/Portfolio2/static/js/main.6009e555.chunk.js"
+  },
+  {
+    "revision": "a001d0716b2d97b57bf2",
+    "url": "/Portfolio2/static/js/1.a001d071.chunk.js"
+  },
+  {
+    "revision": "6009e555e7a58898f77d",
+    "url": "/Portfolio2/static/css/main.7d912096.chunk.css"
+  },
+  {
+    "revision": "f2864ad8e18f41885b0749f0326c2c71",
+    "url": "/Portfolio2/index.html"
+  }
+];
