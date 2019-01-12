158c8d85d46d46ef72a4a84bf98b13c63127f0c8
Xurmein <wj.pavocrist@gmail.com>
2019-01-12 15:57:49 -0500

    re-doing resume link for simplicity

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import {
-    Button
-} from 'reactstrap';
-import {
     Route,
     Link,
     Switch
 } from "react-router-dom";
+import {
+    Button
+} from 'reactstrap';
 import LandingPage from './site/LandingPage';
 import AboutMe from './site/AboutMe'
 import Projects from './site/Projects'
 import ContactMe from './site/ContactMe';
+import Resume from './Resume';
 import '../App.css';
 
-const Navigator = () => (
+const Navigator = (props) => (
     <div className="navigator">
         <section className="nav">
             <ul className="nav-links">
@@ -45,6 +46,11 @@ const Navigator = () => (
                         </Button>
                     </Link>
                 </li>
+                <li>
+                    <a href="./JAKE_PEACOCK_RESUME.docx.pdf" target="_blank">
+                        <Resume/>
+                    </a>
+                </li>
             </ul>
         </section>
         <div className="routes">
@@ -53,6 +59,7 @@ const Navigator = () => (
                 <Route exact path="/aboutme"><AboutMe/></Route>
                 <Route exact path="/projects"><Projects/></Route>
                 <Route exact path="/contactme"><ContactMe/></Route>
+                <Route exact path="_blank"><Resume/></Route>
                 <Route path="/"><LandingPage/></Route>
             </Switch>
         </div>
---
+++ src/components/Resume.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+// import { 
+//     Modal,
+//     Button 
+// } from 'reactstrap';
+
+// class Resume extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             modal : false
+//         };
+//         this.toggle = this.toggle.bind(this);
+//     }
+
+//     onClick = (event) => {
+//         let resume = './JAKE_PEACOCK_RESUME.docs.pdf';
+//         document.open(resume, '_blank')
+//         event.preventDefault();
+//     } 
+    
+
+//     render() {
+//         return (
+//             <Button type="submit" onSubmit={this.onClick} className="nav-button">
+//                 .pdf Resume
+//             </Button>
+//         );
+    
+//     }
+// }
+
+// export default Resume;
